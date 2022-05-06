eea7b8d866174756c0b2e80c9d1d898656d21f9b
eunbean <ds5bean23@gmail.com>
2022-05-06 17:37:27 +0900

    notice-try-catch

--- routes/Notice.js
+++ routes/Notice.js
@@ -1,50 +1,52 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { Notice } = require('../models');
-const multer = require('multer');
-const fs = require('fs');
-const bodyParser = require('body-Parser');
+const { Notice } = require("../models");
+const multer = require("multer");
+const fs = require("fs");
+const bodyParser = require("body-Parser");
 
-router.use(bodyParser.json())
+router.use(bodyParser.json());
 
-router.get("/", async(req,res) => {
-    const listOfNotice = await Notice.findAll()
-    res.json(listOfNotice);
+router.get("/", async (req, res) => {
+  const listOfNotice = await Notice.findAll();
+  res.json(listOfNotice);
 });
 
-router.get('/notices/:id', async(req,res) => {
-    const id = req.params.id;
-    const notice = await Notice.findByPk(id);
-    res.json(notice);
+router.get("/notices/:id", async (req, res) => {
+  const id = req.params.id;
+  const notice = await Notice.findByPk(id);
+  res.json(notice);
 });
 
-router.delete("/:noticeId", async(req,res) => {
-    const noticeId = req.params.noticeId;
+router.delete("/:noticeId", async (req, res) => {
+  const noticeId = req.params.noticeId;
+  try {
     await Notice.destroy({
-        where: {
-            id: noticeId,
-        },
+      where: {
+        id: noticeId,
+      },
     });
     res.json("DELETE NOTICE POST");
+  } catch (err) {
+    res.json("ERROR DELETE");
+  }
 });
 
-
-
 try {
-    fs.readdirSync('uploads');
+  fs.readdirSync("uploads");
 } catch (error) {
-    console.error('uploads 폴더가 없어 uploads 폴더를 생성합니다.');
-    fs.mkdirSync('uploads');
-} 
+  console.error("uploads 폴더가 없어 uploads 폴더를 생성합니다.");
+  fs.mkdirSync("uploads");
+}
 
 const randomstring = require("randomstring");
 
 const upload = multer({
   storage: multer.diskStorage({
     destination: (req, file, cb) => {
-      cb(null, 'uploads/'); //저장할 폴더
+      cb(null, "uploads/"); //저장할 폴더
     },
-    filename: (req, file, cb) => {     
+    filename: (req, file, cb) => {
       var fileName = randomstring.generate(25);
       var mimeType;
       switch (
@@ -67,7 +69,7 @@ const upload = multer({
           break;
       }
       //count++;
-      cb(null, fileName+"." + mimeType); // 파일 이름 + 파일 타입 형태로 이름 설정
+      cb(null, fileName + "." + mimeType); // 파일 이름 + 파일 타입 형태로 이름 설정
     },
   }),
   limits: {
@@ -75,36 +77,35 @@ const upload = multer({
   },
 });
 
-
 router.post("/", upload.single("noImg"), async (req, res) => {
-
-    const { noTitle,
-    noText,
-    noTag } = req.body;
-    const noImg = req.file.path;
+  const { noTitle, noText, noTag } = req.body;
+  const noImg = req.file.path;
   //각종 예외처리 생략
-  
-    const all = await Notice.create({
-        noTitle,
-        noText,
-        noTag,
-        noImg,
-    });
-    res.json(all);
+
+  const all = await Notice.create({
+    noTitle,
+    noText,
+    noTag,
+    noImg,
+  });
+  res.json(all);
 });
 
-router.put("/update",upload.single("noImg"), async(req,res) => {
-    await Notice.update({
-        noTitle:req.body.noTitle,
-        noText:req.body.noText,
-        noTag:req.body.noTag,
-        noImg:req.file.path},{
-        where:{
-            id:req.body.id
-        }}
-    );
-    res.json("UPDATE NOTICE POST");
+router.put("/update", upload.single("noImg"), async (req, res) => {
+  await Notice.update(
+    {
+      noTitle: req.body.noTitle,
+      noText: req.body.noText,
+      noTag: req.body.noTag,
+      noImg: req.file.path,
+    },
+    {
+      where: {
+        id: req.body.id,
+      },
+    }
+  );
+  res.json("UPDATE NOTICE POST");
 });
 
-
-module.exports = router
+module.exports = router;
